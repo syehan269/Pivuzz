5dc3ae42d848c4c6db676776b2da0ec944950ba8
syehan269 <syehan269@gmail.com>
2020-05-12 00:26:56 +0700

    report finished

--- app/src/main/java/com/syehan/pivuzz/ActivityDefault.kt
+++ app/src/main/java/com/syehan/pivuzz/ActivityDefault.kt
@@ -1,7 +1,9 @@
 package com.syehan.pivuzz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.google.android.material.tabs.TabLayout
@@ -15,19 +17,30 @@ class ActivityDefault : AppCompatActivity() {
         val tabAdapter = TabAdapter(supportFragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
         viewPager.adapter = tabAdapter
 
+        toolbarDef.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.about ->{
+                    startActivity(Intent(this, ActivityAbout::class.java))
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.setting ->{
+                    startActivity(Intent(this, ActivitySetting::class.java))
+                    return@setOnMenuItemClickListener true
+                }
+                else -> return@setOnMenuItemClickListener false
+            }
+
+        }
+
         setTab()
     }
 
     private fun setTab() {
         viewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabMain))
         tabMain!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabReselected(tab: TabLayout.Tab?) {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
 
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
 
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 viewPager!!.currentItem = tab!!.position
--- app/src/main/java/com/syehan/pivuzz/ActivitySetting.kt
+++ app/src/main/java/com/syehan/pivuzz/ActivitySetting.kt
@@ -2,12 +2,16 @@ package com.syehan.pivuzz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class ActivitySetting : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        setSupportActionBar(toolbarSet)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
     }
 
 }
